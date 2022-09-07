aa755c8affa1839e4236c0d559eb6628d3bc5f51
Arkes Ray <arkesray@gmail.com>
2022-09-07 23:04:25 +0530

    added .env variables

--- Tracker_API/config.py
+++ Tracker_API/config.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY=os.environ.get("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from Tracker_API import create_app
 
-app = create_app(debug=True)
+app = create_app(debug=False)
 
 if __name__ == '__main__':
     app.run( host="0.0.0.0")
